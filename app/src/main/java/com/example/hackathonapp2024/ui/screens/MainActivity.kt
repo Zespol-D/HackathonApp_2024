372812ff17b2abef26db7f269e419f5c40087710
RadoslawStanczak <radoslaw.stanczak2001@gmail.com>
2024-10-09 15:03:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hackathonapp2024/ui/screens/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hackathonapp2024.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.hackathonapp2024.ui.theme.HackathonApp2024Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HackathonApp2024Theme {
+               Surface {
+
+               }
+            }
+        }
+    }
+}
