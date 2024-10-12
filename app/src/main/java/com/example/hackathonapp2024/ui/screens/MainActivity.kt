95fc49e2fe1b844211dd3ced4f87437b07b4ea6b
BartoszBedyk <bartoszbedyk@gmail.com>
2024-10-12 15:58:39 +0200

    Zmiany z inpections

--- app/src/main/java/com/example/hackathonapp2024/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/hackathonapp2024/ui/screens/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
